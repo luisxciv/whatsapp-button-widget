dc24619f1d2dbab2f62b04733184864cce1a89b2
Luis <luisxciv@gmail.com>
2020-06-01 22:46:13 -0500

    2.5

--- whatsapp-button-widget.js
+++ whatsapp-button-widget.js
@@ -22,7 +22,7 @@ class LeadsalesWhatsAppBtn {
         document.body.appendChild(roboto)
 
         let styles = document.createElement('link')
-        styles.setAttribute('href', 'https://cdn.jsdelivr.net/gh/luisxciv/whatsapp-button-widget@2.1/whatsapp-button-widget.css')
+        styles.setAttribute('href', 'https://cdn.jsdelivr.net/gh/luisxciv/whatsapp-button-widget@2.5/whatsapp-button-widget.css')
 
         styles.setAttribute('rel', 'stylesheet')
         styles.setAttribute('type', 'text/css')
